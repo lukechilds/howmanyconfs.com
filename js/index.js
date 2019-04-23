33f6332bfae7b94a441da661881832713b62fd57
Luke Childs <lukechilds123@gmail.com>
2019-04-23 19:39:10 +0700

    Prefer coinlist names

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 /* eslint-env browser */
 import 'babel-polyfill'; // eslint-disable-line import/no-unassigned-import
 import escapeHTML from 'escape-html';
+import coinlist from 'coinlist';
 import {version} from '../package';
 import getCoinData from './get-coin-data';
 import formatSeconds from './format-seconds';
@@ -21,7 +22,7 @@ getCoinData().then(coins => {
 		<tbody>
 			${coins.map(coin => `
 			<tr>
-				<td>${escapeHTML(`${coin.name} (${coin.symbol})`)}</td>
+				<td>${escapeHTML(`${coinlist.get(coin.symbol, 'name') || coin.name} (${coin.symbol})`)}</td>
 				<td>${escapeHTML(`${coin.algorithm} @ ${coin.hashRateFormatted}`)}</td>
 				<td>${escapeHTML(coin.confirmations.toLocaleString())} confs</td>
 				<td>${escapeHTML(formatSeconds(coin.estimatedTimeForConfs))}</td>
