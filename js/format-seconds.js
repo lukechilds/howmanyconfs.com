8e02651222d8a59944e429396c66d58b6af0ac44
Luke Childs <lukechilds123@gmail.com>
2019-04-23 18:10:37 +0700

    Extract formatSeconds

---
+++ js/format-seconds.js
@@ -0,0 +1,12 @@
+/* eslint-env browser */
+import prettyMs from 'pretty-ms';
+
+const formatSeconds = seconds => {
+	const ms = seconds * 1000;
+	const pretty = prettyMs(ms);
+
+	// Only return first two periods
+	return pretty.split(' ').slice(0, 2).join(' ')
+};
+
+export default formatSeconds;
--- js/index.js
+++ js/index.js
@@ -3,11 +3,12 @@ import 'babel-polyfill'; // eslint-disable-line import/no-unassigned-import
 import prettyMs from 'pretty-ms';
 import {version} from '../package';
 import getCoinData from './get-coin-data';
+import formatSeconds from './format-seconds';
 
 document.querySelector('.version').innerText = `v${version}`;
 
 getCoinData().then(coins => {
 	coins.forEach(({symbol, confirmations, estimatedTimeForConfs, multiplier}) => {
-		console.log(`${symbol}: ${confirmations} confs | ${prettyMs(estimatedTimeForConfs * 1000).split(' ').reduce((str, section, index) => index < 2 ? str + ' ' + section : str)} | ${Math.round(multiplier)}x slower`);
+		console.log(`${symbol}: ${confirmations} confs | ${formatSeconds(estimatedTimeForConfs)} | ${Math.round(multiplier)}x slower`);
 	});
 });
