72c0869b5ec23fa050544c8c8474035a1f0fad3b
Luke Childs <lukechilds123@gmail.com>
2019-04-23 18:40:04 +0700

    Show results in table

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 /* eslint-env browser */
 import 'babel-polyfill'; // eslint-disable-line import/no-unassigned-import
+import escapeHTML from 'escape-html';
 import {version} from '../package';
 import getCoinData from './get-coin-data';
 import formatSeconds from './format-seconds';
@@ -7,7 +8,26 @@ import formatSeconds from './format-seconds';
 document.querySelector('.version').innerText = `v${version}`;
 
 getCoinData().then(coins => {
-	coins.forEach(({symbol, confirmations, estimatedTimeForConfs, multiplier}) => {
-		console.log(`${symbol}: ${confirmations} confs | ${formatSeconds(estimatedTimeForConfs)} | ${Math.round(multiplier)}x slower`);
-	});
+	const table = document.querySelector('table.results');
+
+	table.innerHTML = `
+		<thead>
+			<td>Name</td>
+			<td>Hash Rate</td>
+			<td>Equivalent Confirmations</td>
+			<td>Estimated Time</td>
+			<td>Speed</td>
+		</thead>
+		<tbody>
+			${coins.map(coin => `
+			<tr>
+				<td>${escapeHTML(`${coin.name} (${coin.symbol})`)}</td>
+				<td>${escapeHTML(`${coin.algorithm} @ ${coin.hashRateFormatted}`)}</td>
+				<td>${escapeHTML(coin.confirmations)} confs</td>
+				<td>${escapeHTML(formatSeconds(coin.estimatedTimeForConfs))}</td>
+				<td>${coin.symbol === 'BTC' ? '-' : `${escapeHTML(Math.round(coin.multiplier))} x slower`}</td>
+			</tr>
+			`).join('')}
+		</tbody>
+	`;
 });
