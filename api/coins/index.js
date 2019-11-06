0a88543372d28cc1be72f40bd488b29de2c7ffdc
Luke Childs <lukechilds123@gmail.com>
2019-11-06 22:46:43 +0700

    Add coins API endpoint

---
+++ api/coins/index.js
@@ -0,0 +1,35 @@
+const got = require('got');
+
+const getWtmEndpoint = async endpoint => {
+	const response = await got('https://whattomine.com/' + endpoint, {json: true});
+
+	return response.body;
+};
+
+const getCoinData = async () => {
+	const [gpu, asic] = await Promise.all([
+		getWtmEndpoint('coins.json'),
+		getWtmEndpoint('asic.json')
+	]);
+
+	const coins = Object.entries({...gpu.coins, ...asic.coins})
+		.filter(([_, coin]) => coin.tag !== 'NICEHASH')
+		.map(([name, coin]) => {
+			return {
+				symbol: String(coin.tag),
+				name: String(name),
+				marketCap: parseFloat(coin.market_cap.replace(/[$,]/g, ''), 10),
+				price: parseFloat(coin.exchange_rate),
+				priceCurrency: String(coin.tag === 'BTC' ? 'USD' : coin.exchange_rate_curr),
+				algorithm: String(coin.algorithm),
+				hashrate: parseFloat(coin.nethash, 10),
+				blockTimeInSeconds: parseFloat(coin.block_time, 10),
+				blockReward: parseFloat(coin.block_reward, 10)
+			};
+		})
+		.sort((a, b) => b.marketCap - a.marketCap);
+
+	return coins;
+};
+
+module.exports = getCoinData;
