13dd49762b6feaccc1f722aba6e0920d0522be13
Luke Childs <lukechilds123@gmail.com>
2019-11-08 17:56:12 +0700

    Refactor formatDollars()

--- js/format-dollars.js
+++ js/format-dollars.js
@@ -1,13 +1,12 @@
 const formatDollars = amount => {
-	if (amount >= 1e9) {
-		amount = (amount / 1e9).toFixed(2) + ' B';
-	} else if (amount >= 1e6) {
-		amount = (amount / 1e6).toFixed(2) + ' M';
-	} else {
-		amount = Math.floor(amount).toLocaleString();
+	const units = [[1e9, 'B'], [1e6, 'M']];
+	for (const [devisor, suffix] of units) {
+		if (amount >= devisor) {
+			return `$${(amount / devisor).toFixed(2)} ${suffix}`;
+		}
 	}
 
-	return '$' + amount;
-}
+	return `$${Math.floor(amount).toLocaleString()}`;
+};
 
 export default formatDollars;
