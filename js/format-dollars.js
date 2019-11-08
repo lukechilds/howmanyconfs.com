03b0e2e49184f467401879df4ef7ddd3c25be12b
Luke Childs <lukechilds123@gmail.com>
2019-11-08 17:56:52 +0700

    Add support for trillion dollar market cap formatting

--- js/format-dollars.js
+++ js/format-dollars.js
@@ -1,5 +1,5 @@
 const formatDollars = amount => {
-	const units = [[1e9, 'B'], [1e6, 'M']];
+	const units = [[1e12, 'T'], [1e9, 'B'], [1e6, 'M']];
 	for (const [devisor, suffix] of units) {
 		if (amount >= devisor) {
 			return `$${(amount / devisor).toFixed(2)} ${suffix}`;
